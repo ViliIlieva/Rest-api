610ae6e076e0c556f3230345ef5c6dba455074fe
Velislava <100292906+ViliIlieva@users.noreply.github.com>
2024-01-24 17:45:45 +0200

    Add files via upload

---
+++ router/tasks.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const { auth } = require('../utils');
+const { taskController, commentController } = require('../controllers');
+
+// middleware that is specific to this router
+
+router.get('/', taskController.getTasks);
+router.post('/', auth(), taskController.createTask);
+
+router.get('/:taskId', taskController.getTask);
+router.post('/:taskId', auth(), commentController.createComment);
+router.put('/:taskId', auth(), taskController.subscribe);
+router.put('/:taskId/comments/:commentId', auth(), commentController.editComment);
+router.delete('/:taskId/comments/:commentId', auth(), commentController.deleteComment);
+
+module.exports = router
